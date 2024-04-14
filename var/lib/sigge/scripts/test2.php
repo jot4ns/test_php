4ecda68287d5a5fd158c3de6075c92a17709f168
Jaime Norambuena Sagredo <148829129+jot4ns@users.noreply.github.com>
2024-04-14 16:08:31 -0400

    Update test2.php

--- var/lib/sigge/scripts/test2.php
+++ var/lib/sigge/scripts/test2.php
@@ -1,1 +1,22 @@
+<?php
 
+define('constante', 'ValorConstante');
+
+function testFunction($param)
+{
+    if ($param== 'test'){
+echo "param test";
+}
+    global $otraVar;
+    $varNoUsada = 23;
+    $otraVar = "BlaBla";
+    $fecha = split(":", "2023:04:14");
+    $nuevoValor = & $param;
+    for ($i = 0; $i<10;$i++){
+        echo $i; // Espaciado y formato inconsistentes
+    }
+    return $nuevoValor;
+}
+testFunction("test");
+
+echo "<br>Blablabla bla1";
