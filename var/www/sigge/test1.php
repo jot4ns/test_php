b9b9d9e7ab46375a38302f3378c3c380a0abe0fe
Jaime Norambuena Sagredo <148829129+jot4ns@users.noreply.github.com>
2024-04-14 15:47:00 -0400

    Update test1.php

--- var/www/sigge/test1.php
+++ var/www/sigge/test1.php
@@ -9,3 +9,14 @@ echo "param test";
 }
     global $otraVar;
     $varNoUsada = 23;
+    $otraVar = "BlaBla";
+    $fecha = split(":", "2023:04:14");
+    $nuevoValor = & $param;
+    for ($i = 0; $i<10;$i++){
+        echo $i; // Espaciado y formato inconsistentes
+    }
+    return $nuevoValor;
+}
+testFunction("test");
+
+echo "<br>Blablabla bla1";
