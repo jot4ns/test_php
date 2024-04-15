59f90be8dd7136dca5fe8dc9cf2956a6a24cca13
Jaime Norambuena Sagredo <148829129+jot4ns@users.noreply.github.com>
2024-04-15 13:56:12 -0400

    Update test3.php

--- var/lib/sigge/plantillas_reportes/test3.php
+++ var/lib/sigge/plantillas_reportes/test3.php
@@ -4,16 +4,16 @@ define('constante', 'ValorConstante');
 
 function testFunction($param)
 {
-    if ($param== 'test'){
-echo "param test";
-}
+    if ($param = 'test')
+    echo "param test";
+    }
     global $otraVar;
     $varNoUsada = 23;
     $otraVar = "BlaBla";
     $fecha = split(":", "2023:04:14");
     $nuevoValor = & $param;
     for ($i = 0; $i<10;$i++){
-        echo $i; // Espaciado y formato inconsistentes
+        echo $i;
     }
     return $nuevoValor;
 }
