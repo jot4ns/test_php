c1f85227e2e4158af66cd9f7cd39798efac6700a
Jaime Norambuena Sagredo <jnorambuenasagredo94@gmail.com>
2024-04-22 13:20:38 -0400

    subiendo cambios

--- sql/mantis.sql
+++ sql/mantis.sql
@@ -1,2 +1,2 @@
 SELECT * from empresa;
-UPDAT empresa asdasffas empresa_id = 423 frsom empresa wfre;
+UPDAET empresa asdasffas empresa_id = 423 frsom empresa wfre;
--- var/lib/sigge/scripts/test2.php
+++ var/lib/sigge/scripts/test2.php
@@ -4,7 +4,7 @@ define('constante', 'ValorConstante');
 
 function testFunction($param)
 {
-    if ($param== 'test'){
+    if ($param== 'asdasasd'){
 echo "param test";
 }
     global $otraVar;
