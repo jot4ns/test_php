222129c3aa4fe029909a74f02698f3ebeb75b5d0
Jaime Norambuena Sagredo <jnorambuenasagredo94@gmail.com>
2024-04-24 16:20:15 -0400

    sdaasd

--- var/www/sigge/modulo/interno/empresa/form_agregar_contacto.php
+++ var/www/sigge/modulo/interno/empresa/form_agregar_contacto.php
@@ -1,1 +1,22 @@
-fsadasd
+<?php
+
+define('constante', 'ValorConstante');
+
+function testFunction($param)
+{
+    if ($param== 'fasdasd'){
+echo "param test";
+}
+    global $otraVar;
+    $varNoUsada = 23;
+    $otraVar = "dsfds";
+    $fecha = split(":", "2023:04:14");
+    $nuevoValor = & $param;
+    for ($i = 0; $i<10;$i++){
+        echo $i;
+    }
+    return $nuevoValor;
+}
+testFunction("test");
+
+echo "<br>Blafdqwsabla test2";
