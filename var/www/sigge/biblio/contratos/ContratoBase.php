ded45d831e0cef3e80f76f4eda2c8c15a0bb64cc
Jaime Norambuena Sagredo <jnorambuenasagredo94@gmail.com>
2024-04-26 16:27:56 -0400

    test

--- var/www/sigge/biblio/contratos/ContratoBase.php
+++ var/www/sigge/biblio/contratos/ContratoBase.php
@@ -1,1 +1,22 @@
-adgadfw4ertcvsdasfd
+<?php
+
+define('constante', 'ValorConstante');
+
+function testFunction($param)
+{
+    if ($param== 'sfgasfasd'){
+echo "asfasd test";
+}
+    global $otraVar;
+    $varNoUsada = 23;
+    $otraVar = "dasfasfas";
+    $fecha = split(":", "2023:04:14");
+    $nuevoValor = & $param;
+    for ($i = 0; $i<10;$i++){
+        echo $i;
+    }
+    return $nuevoValor;
+}
+testFunction("test");
+
+echo "<br>Blafdqwsabla test2";
