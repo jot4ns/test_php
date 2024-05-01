5a9ee0323b96dc02c315d67f7c82f202d7033518
Jaime Norambuena Sagredo <148829129+jot4ns@users.noreply.github.com>
2024-04-30 20:15:52 -0400

    Update mantis.sql

--- sql/mantis.sql
+++ sql/mantis.sql
@@ -1,4 +1,4 @@
-INSERT INTO contrato (contrato_id, empresa_vendedora_id, empresa_compradora_id, nombre, clase, licitacion_id, contrato_codigo_compradora, fecha_inicio, fecha_fin, hes_en_detalle, requiere_oc_inicial, posible_nota_credito, flujo_aprobacion, fecha_firma_contrato, reporte_ignorar_facturables, reporte_ignorar_no_facturables, enviar_avisos_flujo, activo)
+IN SERT INTO contrato (contrato_id, empresa_vendedora_id, empresa_compradora_id, nombre, clase, licitacion_id, contrato_codigo_compradora, fecha_inicio, fecha_fin, hes_en_detalle, requiere_oc_inicial, posible_nota_credito, flujo_aprobacion, fecha_firma_contrato, reporte_ignorar_facturables, reporte_ignorar_no_facturables, enviar_avisos_flujo, activo)
 VALUES ((SELECT MAX(contrato_id) FROM contrato)+1, 
 (SELECT empresa_id FROM empresa_receptora WHERE nombre_corto_cdec = 'LIPIGAS'),
 (SELECT empresa_id FROM empresa_receptora WHERE nombre_corto_cdec = 'TONELERIA NACIONAL'), 
